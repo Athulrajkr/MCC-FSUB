3a2554e47dbe333d845273337bd854a57b64784e
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-06-16 07:53:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1325,8 +1325,7 @@ async def auto_filter(client, msg, spoll=False):
     if not spoll:
         message = msg
         settings = await get_settings(message.chat.id)
-        if message.text and message.text.startswith("/"):
-            return
+        if message.text.startswith("/"): return
         if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
             return
         if len(message.text) < 100:
