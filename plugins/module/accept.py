91c0912d9d91b7236400e09c5fcd25b94b9f1f15
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-06-18 04:45:12 +0530

    Update accept.py

--- plugins/module/accept.py
+++ plugins/module/accept.py
@@ -1,12 +1,15 @@
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import filters, Client, errors, enums
 from database.users_chats_db import db
 from info import AUTH_CHANNEL
-
+ 
 
 @Client.on_chat_join_request(filters.group | filters.channel)
 async def approve(bot, m: Message):
-    
+    btn = [[
+        InlineKeyboardButton('🎥 𝐆𝐫𝐨𝐮𝐩 🎥', url='https://t.me/MM_Archives'),
+        InlineKeyboardButton('🎥 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🎥', url='http://t.me/Movie_Meadia')
+    ]]
     try: 
         if m.chat.id == AUTH_CHANNEL:
             return
@@ -15,8 +18,12 @@ async def approve(bot, m: Message):
             await db.add_chat(m.chat.id, m.chat.title) 
             
         await bot.approve_chat_join_request(m.chat.id, m.from_user.id)
-        await bot.send_message(m.from_user.id, "Hello {}\nWelcome To {}".format(m.from_user.mention, m.chat.title))
-        
+        await bot.send_message(
+            chat_id=m.from_user.id, 
+            text="Hello {}\nWelcome To {}".format(m.from_user.mention, m.chat.title))
+            reply_markup=InlineKeyboardMarkup(btn),
+            parse_mode=enums.ParseMode.HTML
+        )
         if not await db.is_user_exist(m.from_user.id):
             await db.add_user(m.from_user.id, m.from_user.first_name) 
     except errors.PeerIdInvalid as e:
