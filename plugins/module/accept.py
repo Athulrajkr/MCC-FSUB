e890fffcaba1f758150d8acff2133987f84986f8
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-06-17 23:18:05 +0530

    Update accept.py

--- plugins/module/accept.py
+++ plugins/module/accept.py
@@ -1,15 +1,23 @@
 from pyrogram.types import Message
 from pyrogram import filters, Client, errors, enums
 from database.users_chats_db import db
+from info import AUTH_CHANNEL
 
+AUTH_CHANNEL =10073836393
 
 @Client.on_chat_join_request(filters.group | filters.channel)
 async def approve(bot, m: Message):
+    
     try: 
+        if m.chat.id == AUTH_CHANNEL:
+            return
+            
         if not await db.get_chat(message.chat.id):
             await db.add_chat(m.chat.id, m.chat.title) 
+            
         await bot.approve_chat_join_request(message.chat.id, m.from_user.id)
         await bot.send_message(m.from_user.id, "Hello {}\nWelcome To {}".format(m.from_user.mention, m.chat.title))
+        
         if not await db.is_user_exist(m.from_user.id):
             await db.add_user(m.from_user.id, m.from_user.first_name) 
     except errors.PeerIdInvalid as e:
