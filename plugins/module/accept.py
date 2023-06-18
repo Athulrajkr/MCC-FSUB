336ef0f3725ad6dafa343a57f4bf6b3e12ad9594
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-06-18 06:38:59 +0530

    Update accept.py

--- plugins/module/accept.py
+++ plugins/module/accept.py
@@ -2,7 +2,7 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import filters, Client, errors, enums
 from database.users_chats_db import db
 from info import AUTH_CHANNEL
- 
+
 
 @Client.on_chat_join_request(filters.group | filters.channel)
 async def approve(bot, m: Message):
@@ -20,14 +20,13 @@ async def approve(bot, m: Message):
         await bot.approve_chat_join_request(m.chat.id, m.from_user.id)
         await bot.send_message(
             chat_id=m.from_user.id, 
-            text="Hello {}\nWelcome To {}".format(m.from_user.mention, m.chat.title)),
+            text="Hello {}\nWelcome To {}".format(m.from_user.mention, m.chat.title),
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.HTML
         )
         if not await db.is_user_exist(m.from_user.id):
             await db.add_user(m.from_user.id, m.from_user.first_name) 
     except errors.PeerIdInvalid as e:
-       print("user isn't start bot")
+        print("user isn't start bot")
     except Exception as err:
-        print(str(err))    
-      
+        print(str(err))
