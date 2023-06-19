69246d0d456418d4b38a00e1d03d973d95f09b4c
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-06-19 17:34:31 +0530

    Update subscribe.py

--- plugins/module/subscribe.py
+++ plugins/module/subscribe.py
@@ -1,41 +1,47 @@
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ChatJoinRequest
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ChatJoin
 from pyrogram import filters, Client, errors, enums
 from database.users_chats_db import db
 from info import AUTH_CHANNEL
 
-
-@Client.on_chat_join_request(filters.group | filters.channel)
-async def approve(bot, m: ChatJoinRequest):
-    btn = [[
-        InlineKeyboardButton('🎥 𝐆𝐫𝐨𝐮𝐩 🎥', url='https://t.me/MM_Archives'),
-        InlineKeyboardButton('🎥 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🎥', url='http://t.me/Movie_Meadia')
-    ]]
-    try: 
+@Client.on_chat_join(filters.group | filters.channel)
+async def approve(bot, m: ChatJoin):
+    btn = [
+        [
+            InlineKeyboardButton('🎥 𝐆𝐫𝐨𝐮𝐩 🎥', url='https://t.me/MM_Archives'),
+            InlineKeyboardButton('🎥 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🎥', url='http://t.me/Movie_Meadia')
+        ]
+    ]
+    try:
         if m.chat.id == AUTH_CHANNEL:
             user_id = m.from_user.id
             first_name = m.from_user.first_name
             username = m.from_user.username
             date = m.date
             await db.add_req(
-                 user_id=user_id,
-                 first_name=first_name,
-                 username=username,
-                 date=date
-            ) 
+                user_id=user_id,
+                first_name=first_name,
+                username=username,
+                date=date
+            )
             return
-        if not await db.get_chat(m.chat.id):
-            await db.add_chat(m.chat.id, m.chat.title) 
-            
-        await bot.approve_chat_join_request(m.chat.id, m.from_user.id)
+
+        if (m.chat and m.chat.type in {enums.ChatType.GROUP, enums.ChatType.SUPERGROUP}):
+            chat_id = m.chat.id
+            chat_title = m.chat.title
+            if not await db.get_chat(chat_id):
+                await db.add_chat(chat_id, chat_title)
+
+        await bot.approve_chat_join(m.chat.id)
         await bot.send_message(
-            chat_id=m.from_user.id, 
+            chat_id=m.from_user.id,
             text="Hello {}\nWelcome To {}".format(m.from_user.mention, m.chat.title),
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.HTML
         )
+
         if not await db.is_user_exist(m.from_user.id):
-            await db.add_user(m.from_user.id, m.from_user.first_name) 
+            await db.add_user(m.from_user.id, m.from_user.first_name)
     except errors.PeerIdInvalid as e:
-        print("user isn't start bot")
+        print("User isn't start bot")
     except Exception as err:
         print(str(err))
