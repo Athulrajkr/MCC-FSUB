fc40dff042c6475339214b28968499fa9106bf49
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-06-19 07:14:27 +0530

    Update accept.py

--- plugins/module/accept.py
+++ plugins/module/accept.py
@@ -1,19 +1,28 @@
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ChatJoinRequest
 from pyrogram import filters, Client, errors, enums
 from database.users_chats_db import db
 from info import AUTH_CHANNEL
 
 
 @Client.on_chat_join_request(filters.group | filters.channel)
-async def approve(bot, m: Message):
+async def approve(bot, m: ChatJoinRequest):
     btn = [[
         InlineKeyboardButton('🎥 𝐆𝐫𝐨𝐮𝐩 🎥', url='https://t.me/MM_Archives'),
         InlineKeyboardButton('🎥 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🎥', url='http://t.me/Movie_Meadia')
     ]]
     try: 
         if m.chat.id == AUTH_CHANNEL:
+            user_id = m.from_user.id
+            first_name = m.from_user.first_name
+            username = m.from_user.username
+            date = m.date
+            await db.add_req(
+                 user_id=user_id,
+                 first_name=first_name,
+                 username=username,
+                 date=date
+            ) 
             return
-            
         if not await db.get_chat(m.chat.id):
             await db.add_chat(m.chat.id, m.chat.title) 
             
